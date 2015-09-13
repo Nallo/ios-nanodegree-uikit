3938c28070b9e6f79653e8eae896872c1397ce98
Stefano <stefano@sherlock.bike>
2015-09-13 15:32:04 +0200

    Add view layout and constraints.

--- TextFields/TextFields/ViewController.swift
+++ TextFields/TextFields/ViewController.swift
@@ -10,49 +10,23 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
-    // Outlets
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var textField3: UITextField!
-    @IBOutlet weak var textField4: UITextField!
-    @IBOutlet weak var characterCountLabel: UILabel!
+    //TODO: Outlets
     
-    // Text Field Delegate objects
-    let emojiDelegate = EmojiTextFieldDelegate()
-    let colorizerDelegate = ColorizerTextFieldDelegate()
-    let randomColorDelegate = RandomColorTextFieldDelegate()
+    //TODO: Text Field Delegate objects
     
     // Life Cycle Methods
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // set the label to be hidden
-        self.characterCountLabel.hidden = true
         
-        // Set the four delegates
-        self.textField1.delegate = emojiDelegate
-        self.textField2.delegate = colorizerDelegate
-        self.textField4.delegate = randomColorDelegate
-        self.textField3.delegate = self
+        //TODO: Set the four delegates
     }
 
     
-    // Text Field Delegate Methods
+    //TODO Text Field Delegate Methods
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
 
-        // Figure out what the new text will be, if we return true
-        var newText: NSString = textField.text
-        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
-        
-        // hide the label if the newText will be an empty string
-        self.characterCountLabel.hidden = (newText.length == 0)
-        
-        // Write the length of newText into the label
-        self.characterCountLabel.text = String(newText.length)
-        
-        // returning true gives the text field permission to change its text
         return true;
     }
 }
